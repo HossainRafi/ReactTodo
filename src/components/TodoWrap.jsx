d85196bd97a4502a860acabb5a0b05f7da8b3969
HossainRafi <hossainrafi71@gmail.com>
2023-06-17 21:40:17 +0600

    show todo in ui

--- src/components/TodoWrap.jsx
+++ src/components/TodoWrap.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
 
 const TodoWrap = () => {
-   const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = (todo) => {
-      setTodos([
-        ...todos,
-        { id: uuidv4(), task: todo, completed: false, isEditing: false },
-      ]);
-    };
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
   return (
     <div className="TodoWrapper">
       <TodoForm addTodo={addTodo} />
+      <Todo />
     </div>
   );
 };
